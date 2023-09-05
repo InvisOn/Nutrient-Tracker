740cee62a3ebbaab5133a9a05a2572fa46924ad5
InvisOn <ajveendijk@gmail.com>
2023-09-06 10:43:33 +1200

    Fix `ADD INGREDIENT` button
    The entire button was not clickable. Now it is.

--- components/Button.tsx
+++ components/Button.tsx
@@ -11,10 +11,9 @@ const Button: React.FC<Props> = ({
     // todo How to make this minimum size based on the screen so that it is never too big?
     // todo currently the size is controlled by the View that this component is in.
     return (
-        <View
-            style={[styles.buttonContainer, { borderRadius: 3, backgroundColor: "#2196f3" }]}>
+        <View style={{ marginVertical: 10, marginHorizontal: 6 }}>
             <Pressable
-                style={[styles.button]}
+                style={[styles.button, { borderRadius: 3, backgroundColor: "#2196f3" }]}
                 onPress={onPress}>
                 <Text style={[styles.buttonLabel, { fontSize: 15, color: "#ffffff", fontWeight: 'bold' }]}>
                     {label}
@@ -34,14 +33,6 @@ const styles = StyleSheet.create({
     buttonLabel: {
         color: '#fff',
         fontSize: 16,
-    },
-    buttonContainer: {
-        marginVertical: 10,
-        marginHorizontal: 6,
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 3,
-
     },
 });
 
