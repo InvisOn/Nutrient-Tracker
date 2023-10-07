ac69ac1763cd10b410130d53b266d7555f870250
InvisOn <ajveendijk@gmail.com>
2023-10-07 17:57:56 +1300

    Add roundTo()

--- utils/numbers.ts
+++ utils/numbers.ts
@@ -1,8 +1,20 @@
 /**
  * Converts a string to a number using 0 as a fallback.
+ * @param value Value to convert.
+ * @returns Converted value.
  */
 export const toNumber = (value: any): number => {
     const num = Number(value)
 
     return Number.isNaN(num) ? 0 : num
+}
+
+/**
+ * Round a number to the given decimal place.
+ * @param n Number to round.
+ * @param place Decimal place to round to.
+ * @returns Rounded number.
+ */
+export const roundTo = (n: number, place: number) => {
+    return Number(Math.round(Number(String(n) + "e+" + String(place))) + "e-" + place);
 }
