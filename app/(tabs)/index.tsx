8f3b81561a8e066718ff5d231125b4eaf3856249
InvisOn <ajveendijk@gmail.com>
2023-10-09 12:23:00 +1300

    add goals table to db

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet } from 'react-native'
 import { Text, View } from '@/components/Themed'
 
+// todo add progress towards goals to db
 // todo add get database button for db file or csv files
 // todo add nutrition goal
 // todo add progress towards nutrition goal tracking
--- database/databaseContext.ts
+++ database/databaseContext.ts
@@ -1,6 +1,8 @@
+import { consoleLogTimeSqlCallbacks } from "@/utils/debug"
 import { openDatabase, Database, SQLTransaction } from "expo-sqlite"
 import { createContext } from 'react'
 
+// todo If I want to change to field names to be more descriptive (fat -> fat_per_hectogram)it is a hassle to change it everywhere in the code base. Perhaps an ORM can help?
 const createDatabase = (): Database => {
     const database = openDatabase("food.db")
 
@@ -8,27 +10,38 @@ const createDatabase = (): Database => {
         // !! temporary, to prevent the db from ballooning in size when debugging.
         tx.executeSql("DROP TABLE foods;");
         tx.executeSql("DROP TABLE food_consumed;");
+        tx.executeSql("DROP TABLE nutrient_goals;");
 
         tx.executeSql(
-            // todo If I want to change to field names to be more descriptive (fat -> fat_per_hectogram)it is a hassle to change it everywhere in the code base. Perhaps an ORM can help?
             `CREATE TABLE IF NOT EXISTS foods (
                 id_food INTEGER PRIMARY KEY NOT NULL,
                 name TEXT NOT NULL,
                 protein REAL NOT NULL,
                 fat REAL NOT NULL,
                 carbs REAL NOT NULL,
-                energy REAL NOT NULL);`)
+                energy REAL NOT NULL);`
+        )
 
         tx.executeSql(
             `CREATE TABLE IF NOT EXISTS food_consumed (
-                id_consumed INTEGER PRIMARY KEY NOT NULL,
-                id_food INTEGER NOT NULL,
-                grams_consumed REAL NOT NULL,
-            FOREIGN KEY (id_food)
-                REFERENCES foods (id_food));`
+                        id_consumed INTEGER PRIMARY KEY NOT NULL,
+                        id_food INTEGER NOT NULL,
+                        grams_consumed REAL NOT NULL,
+                    FOREIGN KEY (id_food)
+                        REFERENCES foods (id_food));`
         )
 
-        // !! temporary, to fill the table for debugging purposes.
+        tx.executeSql(
+            `CREATE TABLE IF NOT EXISTS nutrient_goals (
+                id_goal INTEGER PRIMARY KEY NOT NULL CHECK (id_goal = 1),
+                grams_protein INTEGER NOT NULL,
+                grams_fat INTEGER NOT NULL,
+                grams_carbs REAL NOT NULL);`
+        )
+
+        // !! temporary, to fill the tables for debugging purposes.
+        tx.executeSql("INSERT INTO nutrient_goals (grams_protein, grams_fat, grams_carbs) VALUES (?, ?, ?);", [1, 2, 3])
+
         for (let i = 1; i <= 20; i++) {
             tx.executeSql("INSERT INTO foods (name, protein, fat, carbs, energy) VALUES (?, ?, ?, ?, ?);", [
                 `Food ${i}`,
@@ -39,7 +52,6 @@ const createDatabase = (): Database => {
             ])
         }
 
-        // !! temporary, to fill the table for debugging purposes.
         for (let i = 1; i <= 20; i++) {
             tx.executeSql("INSERT INTO food_consumed (id_food, grams_consumed) VALUES (?, ?);",
                 [i, i])
