dbb3cc3ebf3c579cbce2116f68d89db10e37e7e8
InvisOn <ajveendijk@gmail.com>
2023-10-08 14:27:34 +1300

    add debug alertTime and debug.ts doc comments

--- utils/debug.ts
+++ utils/debug.ts
@@ -1,38 +1,46 @@
-const consoleLogHelper = (...msg: any) => {
-    const newMsg: any = []
+import * as FileSystem from 'expo-file-system'
+
+/**
+ * Formats the arguments into an `any[]` with the first element the current 24h format time.
+ * It will also tab indent strings ending with colon.
+ * @param msg
+ * @returns
+ */
+const timeFormatHelper = (...msg: any) => {
+    const now = new Date()
+    const hours = String(now.getHours()).padStart(2, '0')
+    const minutes = String(now.getMinutes()).padStart(2, '0')
+    const seconds = String(now.getSeconds()).padStart(2, '0')
+
+    const newMsg: any[] = [`${hours}:${minutes}:${seconds}`]
 
     for (const m of msg) {
         if (typeof m === 'string' && m.slice(-1) === ":") {
             newMsg.push("\n\t" + m)
         } else {
             newMsg.push(m)
-            continue
         }
     }
 
-    const now = new Date()
-    const hours = String(now.getHours()).padStart(2, '0')
-    const minutes = String(now.getMinutes()).padStart(2, '0')
-    const seconds = String(now.getSeconds()).padStart(2, '0')
+    if (newMsg.length > 1) newMsg[0] = newMsg[0] + " -"
 
-    return [`${hours}:${minutes}:${seconds} -`, ...newMsg]
+    return newMsg
 }
 
 /**
- * Logs the message to the console. Will indent strings ending with colon.
+ * Logs the message to the console with the time. Will indent strings ending with colon.
  * @param msg
  */
 export const consoleLogTime = (...msg: any) => {
-    console.log(...consoleLogHelper(...msg))
+    console.log(...timeFormatHelper(...msg))
 }
 
 /**
- * Logs the error message to the console. Will indent strings ending with colon.
+ * Logs the error message to the console the time. Will indent strings ending with colon.
  * @param msg
  */
 export const consoleLogTimeError = (...msg: any) => {
-    console.error(...consoleLogHelper(...msg))
-
+    console.error(...timeFormatHelper(...msg))
 }
 
 /**
@@ -47,4 +55,29 @@ export const consoleLogTimeSqlCallbacks = () => {
             return true
         }
     ]
+}
+
+/**
+ * Show alert the message to the console with the time. Will indent strings ending with colon.
+ * @param msg
+ */
+export const alertTime = (...msg: any) => {
+    alert(timeFormatHelper(...msg))
+}
+
+/**
+ * Checks if the given path relative to `FileSystem.documentDirectory` exists.
+ * @param path Relative path to `FileSystem.documentDirectory`.
+ * @param useConsoleLogTime Obviates the needs to await the promise to log to the console
+ * @param useAlertTime Obviates the needs to await the promise to show an alert.
+ */
+export async function pathExists(path: string, useConsoleLogTime?: boolean, useAlertTime?: boolean) {
+    const exists = (await FileSystem.getInfoAsync(FileSystem.documentDirectory + path)).exists
+
+    const msg = `${path}: ${exists}.`
+
+    if (useConsoleLogTime) consoleLogTime(msg)
+    if (useAlertTime) alertTime(msg)
+
+    return exists
 }
