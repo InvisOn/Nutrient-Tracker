a6a6385faab989c54dcc94075b2f9dba16069de8
InvisOn <ajveendijk@gmail.com>
2023-10-09 15:34:08 +1300

    add doccomment

--- utils/forceRender.ts
+++ utils/forceRender.ts
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 
+/**
+ * `const [forceRenderId, setForceRenderId] = useForceRender()`
+ * @returns [forceRenderId, setForceRenderId]
+ */
 export const useForceRender = (): [number, () => void] => {
     const [value, setValue] = useState(0)
     return [value, () => setValue(value + 1)]
